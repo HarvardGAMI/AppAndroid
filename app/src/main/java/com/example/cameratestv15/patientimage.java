780cd7bf682f7184af90a358ec79def40d69de40
HarvardGAMI <harvardgami@gmail.com>
2019-05-24 19:57:38 -0400

    Fixed it so it successfully saves the image. Appears to work on physical device not emulator. Also previews the image.

--- app/src/main/java/com/example/cameratestv15/patientimage.java
+++ app/src/main/java/com/example/cameratestv15/patientimage.java
@@ -2,6 +2,7 @@ package com.example.cameratestv15;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,39 +24,28 @@ public class patientimage extends AppCompatActivity {
     private ImageView mImageView;
     private static final int REQUEST_IMAGE_CAPTURE = 101;
     static final int REQUEST_TAKE_PHOTO = 1;
-    String currentPhotoPath;
+    String currentPhotoPath = null;
+    File photoFile = null;
 
-//testcommitt
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patientimage);
-
         mImageView = findViewById(R.id.imageView);
-
     }
 
-
+    //code to request camera to take picture and sets up storage of picture
     public void takePicture(View view) {
-        //Log.d("Activity", "Picture taking");
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            Log.d("Activity", "Picture taking");
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-
-        }
-
-    }
-
-   /* public void takePicture(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
-            File photoFile = null;
+            //File photoFile = null;
             try {
                 photoFile = createImageFile();
             } catch (IOException ex) {
+
+                ex.printStackTrace();
                 // Error occurred while creating the File
 
             }
@@ -67,21 +58,22 @@ public class patientimage extends AppCompatActivity {
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
-    }*/
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
-        }
     }
 
-
-
+    //after picture is taken and stored, open up picture and display it in imageView preview
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ImageView ivPreview = (ImageView) findViewById(R.id.imageView);
+                ivPreview.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -98,5 +90,31 @@ public class patientimage extends AppCompatActivity {
         currentPhotoPath = image.getAbsolutePath();
         return image;
     }
+
+
+   //Old code for taking the picture and simply displaying output as a bitmap image
+    /*  public void takePicture(View view) {
+        //Log.d("Activity", "Picture taking");
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            Log.d("Activity", "Picture taking");
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+        }
+    }
+*/
+
+
+    //Old code for taking the captured image and displaying the preview in imageview
+    /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+*/
 
 }
