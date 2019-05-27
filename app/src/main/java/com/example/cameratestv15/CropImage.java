52e069ec5f942a61ec7b6c0b8e93e83a58aca978
HarvardGAMI <harvardgami@gmail.com>
2019-05-27 18:54:41 -0400

    Redid the crop functionality, still doesn't work

--- app/src/main/java/com/example/cameratestv15/CropImage.java
+++ app/src/main/java/com/example/cameratestv15/CropImage.java
@@ -1,5 +1,8 @@
 package com.example.cameratestv15;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,23 +10,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.net.URI;
+
 public class CropImage extends AppCompatActivity {
+
+    Uri uri;
+    Intent cropIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crop_image);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        uri = patientimage.getPhotoURI ();
+        CropFunction();
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void CropFunction () {
+        try
+        {
+            cropIntent = new Intent("com.android.camera.action.CROP");
+            cropIntent.setDataAndType(uri, "image/*");
+            cropIntent.putExtra("crop", true);
+            cropIntent.putExtra("outputX", 180);
+            cropIntent.putExtra("outputY", 180);
+            cropIntent.putExtra("aspectX", 3);
+            cropIntent.putExtra("aspectY", 3);
+            cropIntent.putExtra("scaleUpIfneeded", true);
+            cropIntent.putExtra("return-data", true);
+
+            startActivityForResult(cropIntent, 0);
+        }
+        catch (ActivityNotFoundException e) {
+
+        }
     }
 
 }
